d2d4b9063b5042e81eae352e232ec0e1d5f8dd51
ecknomichiel <michiel@frostvikenit.eu>
2017-06-22 15:31:17 +0200

    Beginning adding transactions by wwriting a testcase

--- TDDBankingTests/AccountTest.cs
+++ TDDBankingTests/AccountTest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
 using TDDBanking.Models;
+using TDDBanking.DataAccess;
 
 namespace TDDBankingTests
 {
     [TestClass]
     public class AccountTest
     {
+
         [TestMethod]
         public void AccountHasNumber()
         {
@@ -22,6 +25,18 @@ namespace TDDBankingTests
         {
             //Arrange
             Account account = new Account() { Balance = 100000000.00 };
+            //Act
+            //Assert
+            Assert.AreEqual(100000000.00, account.Balance);
+        }
+
+        [TestMethod]
+        public void AccountGetTransactionHistoryEmpty()
+        {
+            //Arrange
+            Account account = new Account() { AccountNumber = 13, AddTransaction(new Transaction() {ID = 1, Amount = 1000, BalancedAccountNr = 707}) };
+
+
             //Act
             //Assert
             Assert.AreEqual(100000000.00, account.Balance);
