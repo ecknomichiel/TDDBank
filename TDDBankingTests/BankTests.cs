b514206c01af6e45fd41ed263d8d03d4ee6a7028
ecknomichiel <michiel@frostvikenit.eu>
2017-06-22 15:00:15 +0200

    Added unit tests in BankTests for GetAccountByNumber

--- TDDBankingTests/BankTests.cs
+++ TDDBankingTests/BankTests.cs
@@ -102,5 +102,47 @@ namespace TDDBankingTests
             Assert.IsInstanceOfType(actualResult, typeof(IEnumerable<Account>));
             Assert.IsTrue((expectedResult as IEnumerable<Account>).SequenceEqual<Account>(actualResult));
         }
+
+        [TestMethod]
+        public void GetAccountByExistingNumberGivesAccount()
+        {
+            //Arrange
+            Account expectedResult = new Account() { AccountNumber = 7, Balance = 100 };
+            ICollection<Account> allAccounts = new List<Account>() { 
+                new Account() {AccountNumber = 1},
+                new Account() {AccountNumber = 1},
+                new Account() {AccountNumber = 2},
+                expectedResult
+            };
+            IBankData fakeDb = Mock.Create<IBankData>();
+            Mock.Arrange(() => fakeDb.GetAllAccounts()).Returns(allAccounts);
+            Bank bank = new Bank(fakeDb);
+            //Act
+            Account actualResult = bank.GetAccountByNumber(7);
+            //Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void GetAccountByNonExistingNumberGivesNull()
+        {
+            //Arrange
+            Account expectedResult = null;
+            ICollection<Account> allAccounts = new List<Account>() { 
+                new Account() {AccountNumber = 1},
+                new Account() {AccountNumber = 1},
+                new Account() {AccountNumber = 2},
+                new Account() { AccountNumber = 7, Balance = 100 }
+            };
+            IBankData fakeDb = Mock.Create<IBankData>();
+            Mock.Arrange(() => fakeDb.GetAllAccounts()).Returns(allAccounts);
+            Bank bank = new Bank(fakeDb);
+            //Act
+            Account actualResult = bank.GetAccountByNumber(100);
+            //Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
