8277111a882b4934484e3f2917803b5f9c881bf3
ecknomichiel <michiel@frostvikenit.eu>
2017-06-22 10:22:16 +0200

    Created test for Bank constructor and Bank class.

---
+++ TDDBankingTests/BankTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDBanking.Models;
+
+namespace TDDBankingTests
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void ConstructorGivesBank()
+        {
+            //Arrange
+            
+            //Act
+            Bank actualResult = new Bank();
+            //Assert
+            Assert.IsNotNull(actualResult);
+            Assert.IsInstanceOfType(actualResult, typeof(Bank));
+        }
+    }
+}
